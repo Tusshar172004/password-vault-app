f0103be692604032356c62d2ded58127cceb1d5c
Tushar Patil <tusharpatil17012001@gmail.com>
2025-10-07 18:39:12 +0530

    FIX: Added quotes to production API URL

--- client/src/api.js
+++ client/src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:4000',
-});
+ baseURL: import.meta.env.VITE_API_URL || 'https://password-vault-app-jgob.onrender.com',
+  });
 
 export function setAuthToken(token) {
   if (token) api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
